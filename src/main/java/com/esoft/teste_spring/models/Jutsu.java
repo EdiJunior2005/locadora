47430f16de878bf2a5873e9a4e44b0a751bcd3e7
edi@webfloatcom.br <EdiWF2025.>
2025-06-17 18:31:33 -0300

    feat: base para o projeto

---
+++ src/main/java/com/esoft/teste_spring/models/Jutsu.java
@@ -0,0 +1,29 @@
+package com.esoft.teste_spring.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_jutsu")
+public class Jutsu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    private String dificuldade;
+
+    @ManyToMany(mappedBy = "jutsus")
+    private List<Ninja> ninjas;
+}
---
+++ src/main/java/com/esoft/teste_spring/models/Vila.java
@@ -0,0 +1,28 @@
+package com.esoft.teste_spring.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_vila")
+public class Vila {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "numero_habitantes", nullable = false)
+    private int numeroHabitantes;
+
+    @OneToMany(mappedBy = "vila", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ninja> ninjas;
+}
